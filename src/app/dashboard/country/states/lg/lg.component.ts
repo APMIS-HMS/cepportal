5bf251c85a6de9d03c3fcc4cc0f63fd015659a46
Omoniyi Oluwafemi <femiomoniyi@mettalloids.com>
2018-01-11 01:51:42 +0100

    View LGs

--- src/app/dashboard/country/states/lg/lg.component.ts
+++ src/app/dashboard/country/states/lg/lg.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../../../services/country.service';
 
 @Component({
   selector: 'app-lg',
@@ -8,9 +10,26 @@ import { Location } from '@angular/common';
 })
 export class LgComponent implements OnInit {
 
-  constructor( public location: Location ) { }
+  country;
+  state;
+
+  constructor( public location: Location, public activeRoute: ActivatedRoute, public countryService: CountryService ) { }
 
   ngOnInit() {
+    this.activeRoute.params.subscribe(params => {
+      this.getLGs(params['id'], params['id2']);
+       });
+  }
+
+  getLGs(id, id2) {
+    this.countryService.getState(id)
+      .subscribe(res => {
+        this.country = res;
+        this.state = res.states.filter((obj) => {
+                      return obj._id === id2;
+                    })[0];
+      console.log(this.state);
+      });
   }
 
 
